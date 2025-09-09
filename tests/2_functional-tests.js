dbd723bc9a647d8a09079adf93543c70bf7ed33c
Sothyra12 <schan256@my.centennialcollege.ca>
2025-09-08 23:58:27 -0400

    tested test suite 2 #5

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -89,9 +89,15 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
+      browser.fill('surname', 'Polo').then(() => {
+      browser.pressButton('submit', () => {
+      browser.assert.success();
+      browser.assert.text('span#name', 'Marco');
+      browser.assert.text('span#surname', 'Polo');
+      browser.assert.elements('span#dates', 1);
       done();
+        });
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
