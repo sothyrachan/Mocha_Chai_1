6e95b2cff5860ab29879aa558ac5839aadaa68ac
Sothyra12 <schan256@my.centennialcollege.ca>
2025-09-09 01:00:10 -0400

    Fixed test suite 2 #5

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -89,11 +89,11 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      browser.fill('surname', 'Polo').then(() => {
+      browser.fill('surname', 'Colombo').then(() => {
       browser.pressButton('submit', () => {
       browser.assert.success();
-      browser.assert.text('span#name', 'Marco');
-      browser.assert.text('span#surname', 'Polo');
+      browser.assert.text('span#name', 'Cristoforo');
+      browser.assert.text('span#surname', 'Colombo');
       browser.assert.elements('span#dates', 1);
       done();
         });
